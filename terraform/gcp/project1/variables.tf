9204343a6fcb8a684daf44f73eb83fde5c5838a4
Qiliang Cui <derrhein@gmail.com>
2025-08-04 22:12:47 +0000

    Add capacity.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- terraform/gcp/project1/variables.tf
+++ terraform/gcp/project1/variables.tf
@@ -27,15 +27,15 @@ variable "gcs_bucket" {
 }
 
 variable "v6e_1_count" {
-  default     = 4
+  default     = 8
 }
 
 variable "v6e_4_count" {
-  default     = 0
+  default     = 2
 }
 
 variable "v6e_8_count" {
-  default     = 4
+  default     = 8
 }
 
 variable "branch_hash" {
